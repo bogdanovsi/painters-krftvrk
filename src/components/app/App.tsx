ea5eb1ec31d63f69580a62257d9b781be7dcea0b
Богданов Сергей Иванович <sergey.bogdanov@geropharm.com>
2020-11-20 10:31:29 +0300

    eject

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 
+import Footer from '../Footer';
+
 import logo from './images/logo.png';
 
 const App = () => {
@@ -83,24 +85,7 @@ const App = () => {
           </section>
         </div >
       </main > */}
-      <footer role="contentinfo" className="footer">
-        <div className="container">
-          <div className="footer__content">
-            <article className="footer__article">
-              <h2 className="footer__header">Löydä lähin luotettava tekijä!</h2>
-              <p className="footer__text">Tekijäpankista löydät osaavat ja luotettavat tekijät maalaustyöllesi.</p><a href="/about" className="link footer__link footer__link--more">Lue lisää</a>
-            </article>
-            <ul className="footer__list footer__list--wide">
-              <li className="footer__list-item">
-                <h2 className="footer__header">Yleistä</h2>
-              </li>
-              <li className="footer__list-item"><a href="/about" className="link footer__link">Tietoa palvelusta</a></li>
-              <li className="footer__list-item"><a href="/about/contact" className="link footer__link">Ota yhteyttä</a></li>
-            </ul> 
-          </div>
-          <small className="footer-copy">© 2019 Tikkurila Oyj - <a href="https://www.tikkurila.fi" className="link footer-copy__link">www.tikkurila.fi</a></small>
-        </div>
-      </footer>
+      <Footer />
     </div >
   </>
   );
